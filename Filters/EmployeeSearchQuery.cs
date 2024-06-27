23106f403ad028c0bfc31f8e766704c16454e2e0
Saksham <93081133+fermi6-626@users.noreply.github.com>
2024-06-27 13:52:32 +0545

    Added query filters

---
+++ Filters/EmployeeSearchQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace api.Filters
+{
+    public class EmployeeSearchQuery
+    {
+        public string? Name { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
+
+// Creates the searchable value to pass in params and apply pagination
