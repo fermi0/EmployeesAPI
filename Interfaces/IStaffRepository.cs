af63bc76304127f577261c0f6d22540bade383e6
Saksham <93081133+fermi6-626@users.noreply.github.com>
2024-06-27 13:52:57 +0545

    Applied filters to GetAllAsync

---
+++ Interfaces/IStaffRepository.cs
@@ -0,0 +1,17 @@
+﻿using api.DTO;
+using api.Filters;
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface IStaffRepository // This is an abstration method which is a blueprint for repo to implement
+    {
+        Task<List<Employee>> GetAllAsync(EmployeeSearchQuery query); // This provides filtering
+        Task<Employee?> GetIdAsync(int id);
+        Task<Employee> AddAsync(Employee createEmployee);
+        Task<Employee?> UpdateAsync(int id, UpdateStaffDto editDto);
+        Task<Employee?> DeleteAsync(int id);
+    }
+}
+
+// This is implemented in Repository & returned back by repository & directly into the controller(EmployeesController)
